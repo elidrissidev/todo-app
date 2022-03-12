cbedc5d560137d74321ef5159b5b4169384174ae
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-03-12 11:05:25 +0100

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css'
+import { Header } from '@/components/Header'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <div className="AppContainer">
+        <Header />
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+export function Header() {
+  return (
+    <header className="Header">
+      <h1 className="Header-title">Todo</h1>
+    </header>
+  )
+}
