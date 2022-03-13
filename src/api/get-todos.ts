11db4868a60249d1136d63ab6869f00ed43da9d6
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-03-13 12:10:21 +0100

    Order todos by created_at field

--- src/api/get-todos.ts
+++ src/api/get-todos.ts
@@ -5,6 +5,7 @@ export async function getTodos(): Promise<Todo[]> {
   const res = await apiClient.get<Todo[]>('/rest/v1/todos', {
     params: {
       select: '*',
+      order: 'created_at',
     },
   })
   return res.data
