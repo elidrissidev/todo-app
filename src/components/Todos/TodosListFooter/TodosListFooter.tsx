9bc3957791adec7faf5208fa59fef4144cee9638
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2022-03-20 13:44:13 +0100

    Show filters inside TodosListFooter on desktop

--- src/components/Todos/TodosListFooter/TodosListFooter.tsx
+++ src/components/Todos/TodosListFooter/TodosListFooter.tsx
@@ -10,10 +10,39 @@ export function TodosListFooter({
   itemsLeft = 0,
   completedItems = 0,
 }: TodosListFooterProps) {
-  const { clearCompletedTodos } = useTodos()
+  const { clearCompletedTodos, setFilter, filter } = useTodos()
   return (
     <div className="TodosListFooter">
       <span className="TodosListFooter-items-left">{itemsLeft} items left</span>
+      <div className="TodosListFooter-filters">
+        <button
+          type="button"
+          className="TodosFilter-filter"
+          onClick={() => setFilter('all')}
+          aria-label="Filter by all todos"
+          aria-pressed={filter === 'all'}
+        >
+          All
+        </button>
+        <button
+          type="button"
+          className="TodosFilter-filter"
+          onClick={() => setFilter('active')}
+          aria-label="Filter by active todos"
+          aria-pressed={filter === 'active'}
+        >
+          Active
+        </button>
+        <button
+          type="button"
+          className="TodosFilter-filter"
+          onClick={() => setFilter('completed')}
+          aria-label="Filter by completed todos"
+          aria-pressed={filter === 'completed'}
+        >
+          Completed
+        </button>
+      </div>
       <button
         type="button"
         className="TodosListFooter-clear-completed"
